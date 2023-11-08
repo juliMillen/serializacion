88d687f2d4c414ad537bd73f2aab58d0e3706164
juliMillen <julimillen@gmail.com>
2023-11-08 18:47:47 -0300

    Agregar archivos de proyecto.

---
+++ serializacion/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace serializacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Usuario> lstUsuarios = new List<Usuario>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FileStream fS = null;
+            try
+            {
+                fS = new FileStream("usuarios.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                if (fS.Length > 0)
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    lstUsuarios = (List<Usuario>)formatter.Deserialize(fS);
+                }
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Error en el archivo");
+            }
+            finally
+            {
+                if (fS != null)
+                {
+                    fS.Close();
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            FileStream fS = null;
+            try
+            {
+                fS = new FileStream("usuarios.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(fS, lstUsuarios);
+            }
+            catch(IOException)
+            {
+                MessageBox.Show("Error en el archivo");
+            }
+            finally
+            {
+                if (fS != null)
+                {
+                    fS.Close();
+                }
+            }
+        }
+    }
+}
---
+++ serializacion/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace serializacion
+{
+    [Serializable]
+    class Usuario
+    {
+        public string Nombre { get; set; }
+        public string Contrasenia { get; set; }
+
+        public Usuario(string n, string c)
+        {
+            this.Nombre = n;
+            this.Contrasenia = c;
+        }
+    }
+}
